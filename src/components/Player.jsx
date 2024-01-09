d8e9faed375c994414373b667184411474356176
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2024-01-09 14:27:24 +0100

    prev in the function state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,26 +1,34 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
 
-  function handleEditClick() {
-    setIsEditing(true);
+  function handleChange(event) {
+    // console.log(event);
+    setPlayerName(event.target.value);
   }
 
-  let playerName = <span className="player-name">{name}</span>;
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
 
   if (isEditing) {
-    playerName = <input type="text" required />;
+    editablePlayerName = (
+      <input type="text" onChange={handleChange} required value={playerName} />
+    );
   }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
       </span>
       <button onClick={() => handleEditClick()} className="button">
-        Edit
+        {isEditing ? "Save" : "Edit"}
       </button>
     </li>
   );
